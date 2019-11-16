0de82ad68a1d9db6bbb4de16eef3931187157e50
Artur Lojewski <lojewski@gmail.com>
2019-11-16 12:25:40 +0100

    Initial import

---
+++ src/test/java/org/dukecon/demo/dukecondemo1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dukecon.demo.dukecondemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
